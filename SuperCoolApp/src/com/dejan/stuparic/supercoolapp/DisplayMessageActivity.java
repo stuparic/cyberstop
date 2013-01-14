78f053b150c7dd70c14cbbed44c195471e54af47
ne0 <ne0@ne0-PC>
2013-01-14 21:46:59 +0100

    komentar

--- SuperCoolApp/src/com/dejan/stuparic/supercoolapp/DisplayMessageActivity.java
+++ SuperCoolApp/src/com/dejan/stuparic/supercoolapp/DisplayMessageActivity.java
@@ -3,9 +3,10 @@ package com.dejan.stuparic.supercoolapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.support.v4.app.NavUtils;
+
 
 public class DisplayMessageActivity extends Activity {
 
--- SuperCoolApp/src/com/dejan/stuparic/supercoolapp/MainActivity.java
+++ SuperCoolApp/src/com/dejan/stuparic/supercoolapp/MainActivity.java
@@ -31,3 +31,5 @@ public class MainActivity extends Activity {
 	}
 
 }
+
+//bolji komentar
