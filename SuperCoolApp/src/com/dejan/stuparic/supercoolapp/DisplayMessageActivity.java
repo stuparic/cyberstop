72674fdd3bfff651d9e0db685f94fa76da47241c
koca <koca@koca-VirtualBox>
2013-02-05 22:03:57 +0100

    Ovo je komentar na novoj verziji. Dejan smara.

--- SuperCoolApp/src/com/dejan/stuparic/supercoolapp/DisplayMessageActivity.java
+++ SuperCoolApp/src/com/dejan/stuparic/supercoolapp/DisplayMessageActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.TextView;
-
+//Komentar najnoviji
 //komentar novi
 public class DisplayMessageActivity extends Activity {
 
